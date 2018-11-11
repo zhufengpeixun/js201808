c87af66db3a8c310f3d5e4ff01a39398052e782c
zhufengpeixun <1144709265@qq.com>
2018-11-11 13:01:08 +0800

    2

--- zhufengketang/src/container/Home/HomeLesson.js
+++ zhufengketang/src/container/Home/HomeLesson.js
@@ -5,6 +5,19 @@ export default class HomeLesson extends React.Component{
     }
     render(){
         console.log(this.props.list);
-        return <div></div>
+        return <div className="home-lesson">
+              <div className="home-title">
+                  <span>全部课程</span>
+              </div>
+            {
+                this.props.list.map((item,index)=>{
+                    return <li key={index} className="lesson">
+                        <img src={item.poster} alt=""/>
+                        <p className="price">{item.price}</p>
+                        <p>{item.title}</p>
+                    </li>
+                })
+            }
+        </div>
     }
 }
